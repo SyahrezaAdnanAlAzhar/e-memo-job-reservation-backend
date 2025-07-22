22e348e29dec5d006b8a29cb67ee21471fd820ef
SyahrezaAdnanAlAzhar <syahreza.adnan29@gmail.com>
2025-07-22 11:27:56 +0700

    feat(access permission repo): add update, delete, change status method

--- internal/repository/access_permission_repository.go
+++ internal/repository/access_permission_repository.go
@@ -17,6 +17,10 @@ type CreateAccessPermissionRequest struct {
 	Name string `json:"name" binding:"required"`
 }
 
+type UpdateAccessPermissionRequest struct {
+	Name string `json:"name" binding:"required"`
+}
+
 type AccessPermissionRepository struct {
 	DB *sql.DB
 }
@@ -77,4 +81,65 @@ func (r *AccessPermissionRepository) FindByID(id int) (*AccessPermission, error)
 		return nil, err
 	}
 	return &p, nil
+}
+
+// UPDATE
+func (r *AccessPermissionRepository) Update(id int, req UpdateAccessPermissionRequest) (*AccessPermission, error) {
+	query := `
+        UPDATE access_permission 
+        SET name = $1, updated_at = NOW()
+        WHERE id = $2
+        RETURNING id, name, is_active, created_at, updated_at`
+
+	row := r.DB.QueryRow(query, req.Name, id)
+
+	var updatedPermission AccessPermission
+	err := row.Scan(
+		&updatedPermission.ID, &updatedPermission.Name, &updatedPermission.IsActive,
+		&updatedPermission.CreatedAt, &updatedPermission.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &updatedPermission, nil
+}
+
+// DELETE
+func (r *AccessPermissionRepository) Delete(id int) error {
+	query := "DELETE FROM access_permission WHERE id = $1"
+	result, err := r.DB.Exec(query, id)
+	if err != nil {
+		return err
+	}
+	rowsAffected, _ := result.RowsAffected()
+	if rowsAffected == 0 {
+		return sql.ErrNoRows
+	}
+	return nil
+}
+
+// CHANGE STATUS
+func (r *AccessPermissionRepository) UpdateActiveStatus(id int, isActive bool) error {
+	query := "UPDATE access_permission SET is_active = $1, updated_at = NOW() WHERE id = $2"
+	result, err := r.DB.Exec(query, isActive, id)
+	if err != nil {
+		return err
+	}
+	rowsAffected, _ := result.RowsAffected()
+	if rowsAffected == 0 {
+		return sql.ErrNoRows
+	}
+	return nil
+}
+
+// HELPER
+// CHECK UNIQUE NAME
+func (r *AccessPermissionRepository) IsNameTaken(name string, currentID int) (bool, error) {
+	var exists bool
+	query := "SELECT EXISTS(SELECT 1 FROM access_permission WHERE name = $1 AND id != $2)"
+	err := r.DB.QueryRow(query, name, currentID).Scan(&exists)
+	if err != nil {
+		return false, err
+	}
+	return exists, nil
 }
