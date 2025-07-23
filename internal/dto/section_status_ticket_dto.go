65cc4e8d764b0cd216067a789f8a6934718fa02f
SyahrezaAdnanAlAzhar <syahreza.adnan29@gmail.com>
2025-07-24 03:29:03 +0700

    feat(section status ticket): add dto

--- internal/dto/section_status_ticket_dto.go
+++ internal/dto/section_status_ticket_dto.go
@@ -4,3 +4,7 @@ type CreateSectionStatusTicketRequest struct {
 	Name     string `json:"name" binding:"required"`
 	Sequence int    `json:"sequence" binding:"required"`
 }
+
+type UpdateSectionStatusTicketStatusRequest struct {
+	IsActive bool `json:"is_active"`
+}
