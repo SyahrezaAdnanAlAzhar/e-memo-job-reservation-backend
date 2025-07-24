a667a9711a98e6d5c412f673c5f088af6d012989
SyahrezaAdnanAlAzhar <syahreza.adnan29@gmail.com>
2025-07-24 15:29:06 +0700

    feat(package): add file upload handler

---
+++ pkg/filehandler/filehandler.go
@@ -0,0 +1,33 @@
+package filehandler
+
+import (
+	"fmt"
+	"mime/multipart"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+func SaveFile(c *gin.Context, file *multipart.FileHeader) (string, error) {
+	storagePath := os.Getenv("STORAGE_PATH")
+	if storagePath == "" {
+		storagePath = "./uploads"
+	}
+
+	if err := os.MkdirAll(storagePath, os.ModePerm); err != nil {
+		return "", err
+	}
+
+	extension := filepath.Ext(file.Filename)
+	newFileName := fmt.Sprintf("%d-%s%s", time.Now().UnixNano(), uuid.New().String(), extension)
+	filePath := filepath.Join(storagePath, newFileName)
+
+	if err := c.SaveUploadedFile(file, filePath); err != nil {
+		return "", err
+	}
+
+	return filePath, nil
+}
