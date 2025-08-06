6c5df6f8a0954a933ca066411bd784a326c44238
SyahrezaAdnanAlAzhar <syahreza.adnan29@gmail.com>
2025-08-06 10:57:00 +0700

    feat(job): add handler and route for report upload

--- internal/router/router.go
+++ internal/router/router.go
@@ -184,5 +184,6 @@ func setupMainDataRoutes(group *gin.RouterGroup, h *AllHandlers, r *AllRepositor
 		jobRoutes.GET("/:id/available-actions", h.JobHandler.GetAvailableActions)
 		jobRoutes.PUT("/:id/assign", auth.RequirePermission("JOB_ASSIGN_PIC", r.PositionPermissionRepo), h.JobHandler.AssignPIC)
 		jobRoutes.PUT("/reorder", auth.RequirePermission("JOB_PRIORITY_MANAGE", r.PositionPermissionRepo), h.JobHandler.ReorderJobs)
+		jobRoutes.POST("/:id/report", h.JobHandler.UploadReport)
 	}
 }
