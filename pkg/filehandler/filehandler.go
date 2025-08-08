1c5d3dcb664aed7af3b3e905ec0cb62a9598a12e
SyahrezaAdnanAlAzhar <syahreza.adnan29@gmail.com>
2025-08-08 09:35:16 +0700

    feat(file package): add method to upload multiple file

--- pkg/filehandler/filehandler.go
+++ pkg/filehandler/filehandler.go
@@ -31,3 +31,28 @@ func SaveFile(c *gin.Context, file *multipart.FileHeader) (string, error) {
 
 	return filePath, nil
 }
+
+func SaveFiles(c *gin.Context, files []*multipart.FileHeader) ([]string, error) {
+	storagePath := os.Getenv("STORAGE_PATH")
+	if storagePath == "" {
+		storagePath = "./uploads"
+	}
+
+	if err := os.MkdirAll(storagePath, os.ModePerm); err != nil {
+		return nil, err
+	}
+
+	var filePaths []string
+	for _, file := range files {
+		extension := filepath.Ext(file.Filename)
+		newFileName := fmt.Sprintf("%d-%s%s", time.Now().UnixNano(), uuid.New().String(), extension)
+		filePath := filepath.Join(storagePath, newFileName)
+
+		if err := c.SaveUploadedFile(file, filePath); err != nil {
+			return nil, err
+		}
+		filePaths = append(filePaths, filePath)
+	}
+
+	return filePaths, nil
+}
